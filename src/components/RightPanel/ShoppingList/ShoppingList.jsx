42e57759ed8f162b78887bc6a59d650a0d088843
MICHAL BOGUSZKO <mic.boguszko@gmail.com>
2024-11-27 11:10:52 +0100

    shopping list

--- src/components/RightPanel/ShoppingList/ShoppingList.jsx
+++ src/components/RightPanel/ShoppingList/ShoppingList.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import ShoppingListItem from "./ShoppingListItem/ShoppingListItem";
 import "./ShoppingList.css";
 
 const ShoppingList = (props) => {
+  const [search, setSearch] = useState("");
+  const allItems = [
+    { itemName: "ser", itemQuantity: "1", itemCategory: "Diary Products" },
+    { itemName: "karma", itemQuantity: "1", itemCategory: "Pet Products" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+    { itemName: "dorsz", itemQuantity: "1", itemCategory: "Fish" },
+  ];
+
+  const filteredItems = allItems.filter((item) =>
+    item.itemName.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <>
       <div className="shopping-list-main-container">
@@ -11,21 +31,23 @@ const ShoppingList = (props) => {
               <span className="section-header shopping-list-title">
                 Nazwa listy
               </span>
-              <input type="text" className="shopping-list-search"></input>
+              <input type="text" className="shopping-list-search" value={search} onChange={(e) => setSearch(e.target.value)}></input>
             </div>
             <div className="separator"></div>
           </div>
           <div className="shopping-list-items-container">
-            <ShoppingListItem
-              itemName="test"
-              itemQuantity="5"
-              itemCategory=""
-            ></ShoppingListItem>
-            <ShoppingListItem
-              itemName="test"
-              itemQuantity="5"
-              itemCategory=""
-            ></ShoppingListItem>
+            {filteredItems.length > 0 ? (
+              filteredItems.map((item, index) => (
+                <ShoppingListItem
+                  key={index}
+                  itemName={item.itemName}
+                  itemQuantity={item.itemQuantity}
+                  itemCategory={item.itemCategory}
+                />
+              ))
+            ) : (
+              <p>No items found.</p>
+            )}
           </div>
         </div>
         <div className="products-container"></div>
--- src/components/RightPanel/ShoppingList/ShoppingListItem/ShoppingListItem.jsx
+++ src/components/RightPanel/ShoppingList/ShoppingListItem/ShoppingListItem.jsx
@@ -1,12 +1,22 @@
-import './ShoppingListItem.css';
+import "./ShoppingListItem.css";
+
 const ShoppingListItem = (props) => {
-    const {itemName, itemQuantity,itemCategory} = props;
-    return <>
-        <div className='shopping-list-item-container'>
-            <input type='radio'></input>
-            <span>{itemName}</span>
-            {itemQuantity > 0 && <span>{itemQuantity}</span>}
+  const { itemName, itemQuantity, itemCategory } = props;
+  return (
+    <>
+      <div className="shopping-list-item-container">
+        <div>
+          <input type="checkbox"/>
+          <span>{itemName}</span>
+          {itemQuantity > 1 && <span>{itemQuantity}</span>}
         </div>
+
+        <img
+          src={`${itemCategory.toLowerCase()}.svg`}
+          className="shopping-list-category-img"
+        />
+      </div>
     </>
-}
+  );
+};
 export default ShoppingListItem;
